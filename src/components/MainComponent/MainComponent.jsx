629bb0fad7b140544aba3f697b925dae44b9de9f
iGorillaz <barannik.denis.92@gmail.com>
2022-07-07 20:27:51 +0300

    Simply example of useCallback

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,22 +1,30 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const MainComponent = () => {
-	const [input, setInput] = useState('');
-	const [result, setResult] = useState(0);
-	const [num] = useState(4);
-	const [num1] = useState(5);
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(0);
+  const [num] = useState(4);
+  const [num1] = useState(5);
 
-	useEffect(() => {
-		console.log("Here!");
-	}, [])
+  const sum = useCallback(() => num + num1, [num, num1]);
 
-	return ( 
-		<div>
-			<input type="text" placeholder="input" value={input} onChange={(e) => setInput(e.target.value)} />
-			<h1>Output: {input || '--'}</h1>
-			<p>Result: {JSON.stringify(result)}</p>
-		</div>
-	 );
-}
- 
+  useEffect(() => {
+    console.log(`Here! New sum! ${sum()} `);
+	 setResult(sum())
+  }, [sum]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <h1>Output: {input || "--"}</h1>
+      <p>Result: {JSON.stringify(result)}</p>
+    </div>
+  );
+};
+
 export default MainComponent;
